d0488e8c8066647dbc0506e72608f886123bec8f
hexianWeb <2387213640@qq.com>
2023-02-19 17:44:10 +0800

    feat: 新增 自动按需引入功能

--- vue.config.js
+++ vue.config.js
@@ -6,17 +6,26 @@ const { resolve } = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-    // resolve: {
-    //   alias: {
-    //     views: '@/views',
-    //     components: '@/components'
-    //   }
-    // },
     plugins: [
       AutoImport({
-        resolvers: [ElementPlusResolver()]
+        // 自动按需引入 vue 需要额外的import vue的api。比如使用ref时，需要先import { ref } from 'vue'。
+        imports: ['vue', 'vue-router', 'pinia'],
+        resolvers: [ElementPlusResolver()],
+        // Generate corresponding .eslintrc-auto-import.json file.
+        // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+        eslintrc: {
+          enabled: true, // Default `false`
+          filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+          globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+        }
       }),
       Components({
+        // dirs 指定组件所在位置，默认为 src/components
+        // 可以让我们使用自己定义组件的时候免去 import 的麻烦
+        dirs: ['src/components/'],
+        // 配置需要将哪些后缀类型的文件进行自动按需引入
+        extensions: ['vue'],
+        // 解析的 UI 组件库，这里以 Element Plus 为例
         resolvers: [ElementPlusResolver()]
       })
     ],
